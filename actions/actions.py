e2aae80fb5d98461b14510ba61136ec278efd080
Antonio Igor Carvalho <antonioigorcarvalho@gmail.com>
2024-01-21 15:01:26 -0300

    Extração de título
    Co-Authored-by: LamequeFernandes <lamequesao@gmail.com>

--- actions/actions.py
+++ actions/actions.py
@@ -4,18 +4,18 @@
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
 
-from typing import Any, Text, Dict, List
+# from typing import Any, Text, Dict, List
 
-from rasa_sdk import Action, Tracker
-from rasa_sdk.executor import CollectingDispatcher
-from rasa_sdk.events import SlotSet
+# from rasa_sdk import Action, Tracker
+# from rasa_sdk.executor import CollectingDispatcher
+# from rasa_sdk.events import SlotSet
 
-class ActionSaveUserStorieTitle(Action):
+# class ActionSaveUserStorieTitle(Action):
 
-    def name(self) -> Text:
-        return "action_save_user_storie_title"
+#     def name(self) -> Text:
+#         return "action_save_user_story_title"
 
-    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        title_user_story = tracker.latest_message.get("text")
+#     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#         title_user_story = tracker.latest_message.get("text")
 
-        return [SlotSet("user_storie_title", title_user_story)]
+#         return [SlotSet("user_story_title", title_user_story)]
