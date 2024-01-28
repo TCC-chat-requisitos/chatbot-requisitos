e4606d85113cdae39cc16d127fdc43699716ce21
Antonio Igor Carvalho <antonioigorcarvalho@gmail.com>
2024-01-28 15:56:16 -0300

    Edição de slots
    Co-Authored-by: LamequeFernandes <lamequesao@gmail.com>

--- actions/actions.py
+++ actions/actions.py
@@ -1,21 +1,28 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from typing import Any, Text, Dict, List
 
-# from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
 
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-# from rasa_sdk.events import SlotSet
+# rasa run actions
 
-# class ActionSaveUserStorieTitle(Action):
+class ActionResetUserStoryTitle(Action):
+    def name(self) -> Text:
+        return "action_reset_user_story_title"
 
-#     def name(self) -> Text:
-#         return "action_save_user_story_title"
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        return [SlotSet("user_story_title", None)]
 
-#     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#         title_user_story = tracker.latest_message.get("text")
+class ActionResetUserStoryDescription(Action):
+    def name(self) -> Text:
+        return "action_reset_user_story_description"
 
-#         return [SlotSet("user_story_title", title_user_story)]
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        return [SlotSet("user_story_description", None)]
+    
+class ActionResetUserStoryAcceptanceCriteria(Action):
+    def name(self) -> Text:
+        return "action_reset_user_story_acceptance_criteria"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        return [SlotSet("user_story_acceptance_criteria", None)]
